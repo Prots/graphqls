ff1662a2471e97d8c204df0c67e36d08f49f5d79
Igor Prots <prots.igor@gmail.com>
2016-11-26 22:02:22 +0200

    Fixed timestamp from the beginning of the day

--- service.go
+++ service.go
@@ -8,6 +8,13 @@ import (
 	"time"
 )
 
+const (
+	dateFormat       = "2006-01-02"
+	timeFormat       = "15:04:05"
+	secondsPerMinute = 60
+	secondsPerHour   = 3600
+)
+
 type tm struct {
 	timeStr   string `json:"timeStr"`
 	timestamp int64  `json:"timestamp"`
@@ -84,6 +91,7 @@ func graphqlHandler(w http.ResponseWriter, r *http.Request) {
 		result := executeQuery(queryString["query"][0], schema)
 		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(result)
+		return
 	}
 	w.WriteHeader(http.StatusBadRequest)
 	json.NewEncoder(w).Encode("{\"error\":\"enter valid query string\"}")
@@ -101,7 +109,7 @@ func executeQuery(query string, schema graphql.Schema) *graphql.Result {
 }
 
 func timeStrResolver(p graphql.ResolveParams) (interface{}, error) {
-	return time.Now().Format("15:04:05"), nil
+	return time.Now().Format(timeFormat), nil
 }
 
 func timestampResolver(p graphql.ResolveParams) (interface{}, error) {
@@ -109,11 +117,13 @@ func timestampResolver(p graphql.ResolveParams) (interface{}, error) {
 }
 
 func dateStrResolver(p graphql.ResolveParams) (interface{}, error) {
-	return time.Now().Format("2006-01-02"), nil
+	return time.Now().Format(dateFormat), nil
 }
 
 func timestampOfDateResolver(p graphql.ResolveParams) (interface{}, error) {
-	return time.Now().Unix(), nil
+	dt := time.Now()
+	secondsNow := dt.Second() + dt.Minute()*secondsPerMinute + dt.Hour()*secondsPerHour
+	return secondsNow, nil
 }
 
 func dateObjResolver(p graphql.ResolveParams) (interface{}, error) {
